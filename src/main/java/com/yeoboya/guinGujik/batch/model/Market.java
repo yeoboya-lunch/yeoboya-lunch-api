5734474eb9a312ca7f9de0d779becc6388814993
kimhyunjin <khjzzm@gmail.com>
2022-11-01 11:17:21 +0900

    spring batch

---
+++ src/main/java/com/yeoboya/guinGujik/batch/model/Market.java
@@ -0,0 +1,25 @@
+package com.yeoboya.guinGujik.batch.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Setter
+@Getter
+@Entity
+public class Market {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private int price;
+	
+	@Override
+	public String toString() {
+		return "Market [id=" + id + ", name=" + name + ", price=" + price + "]";
+	}
+}
