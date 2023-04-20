ed162e5671e4201a0463dad94e95aa7f04c91bd3
kimhyunjin <khjzzm@gmail.com>
2023-04-20 17:40:09 +0900

    delete auth

---
+++ src/main/java/com/yeoboya/lunch/config/aspect/AuthAspect.java
@@ -0,0 +1,29 @@
+package com.yeoboya.lunch.config.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Slf4j
+@Aspect
+@Component
+public class AuthAspect {
+
+
+    @Around("@annotation(com.yeoboya.lunch.config.annotation.Auth)")
+    public Object compareJwtTokenSubjectAndRequestBodyEmail(ProceedingJoinPoint pjp) throws Throwable {
+        String proceedName = pjp.getSignature().getDeclaringTypeName() + "." + pjp.getSignature().getName();
+        log.info("[" + proceedName + "] 시작");
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        Object result = pjp.proceed();
+
+        stopWatch.stop();
+        log.info("[" + proceedName + "] 실행시간 : " + stopWatch.getTotalTimeMillis() + " (ms)");
+        return result;
+    }
+}
