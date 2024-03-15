7bdb36ed8f66019b6cafcb5ee184f1b5f5abb309
kimhyunjin <khjzzm@gmail.com>
2024-03-15 18:03:44 +0900

    Update workflows and scripts for ZIP file handling
    This commit streamlines workflow and script adjustments to handle ZIP files directly instead of JARs. The changes include removing synchronization of JAR files to S3 in the GitHub workflow, and updating path handling in the 'appspec.yml' and 'deploy.sh' files. This simplifies the deployment process by eliminating the need to transfer the build files to a different location before unzipping and deploying.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-BUILD_ZIP=$(ls /home/ec2-user/action/*.zip)
+BUILD_ZIP=$(ls /home/ec2-user/build/*.zip)
 ZIP_NAME=$(basename $BUILD_ZIP)
-echo "> Build file name: $ZIP_NAME" >> /home/ec2-user/action/deploy.log
+echo "> Build file name: $ZIP_NAME" >> /home/ec2-user/build/deploy.log
 
-echo "> Check pid of the current application" >> /home/ec2-user/action/deploy.log
+echo "> Check pid of the current application" >> /home/ec2-user/build/deploy.log
 CURRENT_PID=$(pgrep -f .jar)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> No application is currently running, so no shutdown required." >> /home/ec2-user/action/deploy.log
+  echo "> No application is currently running, so no shutdown required." >> /home/ec2-user/build/deploy.log
 else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> Unzip $BUILD_ZIP" >> /home/ec2-user/action/deploy.log
-unzip $BUILD_ZIP -d /home/ec2-user/action/
+echo "> Unzip $BUILD_ZIP" >> /home/ec2-user/build/deploy.log
+unzip $BUILD_ZIP -d /home/ec2-user/build/
 
-DEPLOY_JAR=$(ls /home/ec2-user/action/*.jar)
-echo "> Deploy $DEPLOY_JAR" >> /home/ec2-user/action/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>>/home/ec2-user/action/deploy_err.log &
+DEPLOY_JAR=$(ls /home/ec2-user/build/*.jar)
+echo "> Deploy $DEPLOY_JAR" >> /home/ec2-user/build/deploy.log
+nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>>/home/ec2-user/build/deploy_err.log &
