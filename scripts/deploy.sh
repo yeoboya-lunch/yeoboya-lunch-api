8d9dc5e0f49edb400ca37835af1e051e247944dd
kimhyunjin <khjzzm@gmail.com>
2024-03-15 17:30:07 +0900

    Simplify deploy.sh script and handle ZIP files directly
    The script, deploy.sh, has been streamlined to directly handle ZIP files instead of JAR files. This eliminates the need to copy the build file to a different location before unzipping and deploying. Also, the handling and identification of the ZIP build file and deployment execution have been adjusted accordingly. These efforts align with the recent updates in the build process.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-BUILD_ZIP=$(ls /home/ec2-user/action/build/libs/*.zip)
+BUILD_ZIP=$(ls /home/ec2-user/action/*.zip)
 ZIP_NAME=$(basename $BUILD_ZIP)
 echo "> Build file name: $ZIP_NAME" >> /home/ec2-user/action/deploy.log
-
-echo "> Copy build file" >> /home/ec2-user/action/deploy.log
-DEPLOY_PATH=/home/ec2-user/action/
-cp $BUILD_ZIP $DEPLOY_PATH
 
 echo "> Check pid of the current application" >> /home/ec2-user/action/deploy.log
 CURRENT_PID=$(pgrep -f .jar)
@@ -19,10 +15,9 @@ else
   sleep 5
 fi
 
-DEPLOY_ZIP=$DEPLOY_PATH$ZIP_NAME
-echo "> Unzip $DEPLOY_ZIP" >> /home/ec2-user/action/deploy.log
-unzip $DEPLOY_ZIP -d $DEPLOY_PATH
+echo "> Unzip $BUILD_ZIP" >> /home/ec2-user/action/deploy.log
+unzip $BUILD_ZIP -d /home/ec2-user/action/
 
-DEPLOY_JAR=$(ls $DEPLOY_PATH*.jar)
+DEPLOY_JAR=$(ls /home/ec2-user/action/*.jar)
 echo "> Deploy $DEPLOY_JAR" >> /home/ec2-user/action/deploy.log
 nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>>/home/ec2-user/action/deploy_err.log &
