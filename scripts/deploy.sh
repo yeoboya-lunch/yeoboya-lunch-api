651fd2bf15c4ac787157c99df23ef761d3ff0797
kimhyunjin <khjzzm@gmail.com>
2024-03-15 17:18:20 +0900

    Update deploy.sh to handle ZIP instead of JAR files
    The script for deployment, deploy.sh, has been modified to handle ZIP files instead of JAR files. This involves updates in identifying the build file's name, copying the build file to the deployment path and changes in the application run command. This change corresponds to recent updates in the build process.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
-BUILD_JAR=$(ls /home/ec2-user/action/build/libs/*.jar)
-JAR_NAME=$(basename $BUILD_JAR)
-echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/action/deploy.log
+BUILD_ZIP=$(ls /home/ec2-user/action/build/libs/*.zip)
+ZIP_NAME=$(basename $BUILD_ZIP)
+echo "> Build file name: $ZIP_NAME" >> /home/ec2-user/action/deploy.log
 
-echo "> build 파일 복사" >> /home/ec2-user/action/deploy.log
+echo "> Copy build file" >> /home/ec2-user/action/deploy.log
 DEPLOY_PATH=/home/ec2-user/action/
-cp $BUILD_JAR $DEPLOY_PATH
+cp $BUILD_ZIP $DEPLOY_PATH
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/action/deploy.log
-CURRENT_PID=$(pgrep -f $JAR_NAME)
+echo "> Check pid of the current application" >> /home/ec2-user/action/deploy.log
+CURRENT_PID=$(pgrep -f .jar)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/action/deploy.log
+  echo "> No application is currently running, so no shutdown required." >> /home/ec2-user/action/deploy.log
 else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/action/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/action/deploy_err.log &
+DEPLOY_ZIP=$DEPLOY_PATH$ZIP_NAME
+echo "> Unzip $DEPLOY_ZIP" >> /home/ec2-user/action/deploy.log
+unzip $DEPLOY_ZIP -d $DEPLOY_PATH
+
+DEPLOY_JAR=$(ls $DEPLOY_PATH*.jar)
+echo "> Deploy $DEPLOY_JAR" >> /home/ec2-user/action/deploy.log
+nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>>/home/ec2-user/action/deploy_err.log &
