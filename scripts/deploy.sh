5b8c1d307d5c7f946893927432ac4024319f5f91
kimhyunjin <khjzzm@gmail.com>
2024-03-18 10:22:46 +0900

    Improve deployment script with additional logs and operations
    The deployment script is enhanced to provide more clarity in its logs, and additional operations are included. The script no longer only checks for currently running applications and terminates them, but also adds execution permissions to the new JAR files and completes the process by executing them.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,30 +1,36 @@
 #!/bin/bash
 
+# Repository 저장경로 설정
 REPOSITORY=/home/ec2-user/app
 
-echo "> 현재 구동중인 애플리케이션 pid 확인"
-
+echo -e "\n> 현재 구동중인 애플리케이션 PID 확인"
 CURRENT_PID=$(pgrep -f yeoboya-lunch-api)
+echo -e "> 현재 구동중인 애플리케이션 PID: $CURRENT_PID"
 
-echo "$CURRENT_PID"
-
+# 현재 구동중인 애플리케이션이 있을 경우 종료
 if [ -z $CURRENT_PID ]; then
-    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+    echo -e "\n> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-    echo "> kill -15 $CURRENT_PID"
+    echo -e "\n> kill -15 $CURRENT_PID"
     kill -15 $CURRENT_PID
     sleep 5
 fi
-echo "> 새 어플리케이션 배포"
 
-echo "> 필요한 디렉터리가 있는지 확인 후 생성"
-mkdir -p $REPOSITORY/jar/
+echo -e "\n> 새 어플리케이션 배포"
 
-echo "> Build 파일 복사"
-cp $REPOSITORY/build/libs/*.jar $REPOSITORY/jar/
+# Build 파일 복사
+echo -e "\n> Build 파일 복사"
+cp $REPOSITORY/build/libs/*.jar $REPOSITORY/
 
+# JAR 실행 파일 이름 확인
 JAR_NAME=$(ls -tr $REPOSITORY/jar/ |grep 'yeoboya-lunch-api' | tail -n 1)
-echo "> $JAR_NAME 에 실행권한 추가"
-chmod +x $JAR_NAME
-echo "> JAR Name: $JAR_NAME"
+echo -e "\n> $JAR_NAME 에 실행권한 추가"
+
+# 해당 JAR 파일에 실행권한 추가
+chmod +x $REPOSITORY/jar/$JAR_NAME
+echo -e "\n> JAR_NAME: $JAR_NAME"
+
+# JAR 실행
 nohup java -jar -Dspring.profiles.active=prod $REPOSITORY/jar/$JAR_NAME &
+
+echo -e "\n> JAR 파일 실행 완료"
