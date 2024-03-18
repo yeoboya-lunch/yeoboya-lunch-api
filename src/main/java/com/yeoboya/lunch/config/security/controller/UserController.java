efc459cd1447c6879b0abf502f0b69d91eef6970
kimhyunjin <khjzzm@gmail.com>
2024-03-18 12:07:09 +0900

    Change log path and add system print statement in UserController
    Updated the LOG_PATH in logback-prod.xml from "/home/ec2-user/logs" to "./logs". Added a system output print statement in the signUp method of UserController.java for debugging purposes. Also, the deploy.sh script has been adjusted to output the logs to a different file named "latest-deploy-$DATE.log".

--- src/main/java/com/yeoboya/lunch/config/security/controller/UserController.java
+++ src/main/java/com/yeoboya/lunch/config/security/controller/UserController.java
@@ -37,6 +37,7 @@ public class UserController {
 //    @RateLimited(limit = 1)
     @PostMapping("/sign-up")
     public ResponseEntity<Body> signUp(@Valid @RequestBody SignUp signUp) {
+        System.out.println("system 입니다 . "+ signUp);
         log.info("info 입니다 . {}", signUp);
         log.error("error 입니다 . {}", signUp);
         log.warn("warn 입니다 . {}", signUp);
