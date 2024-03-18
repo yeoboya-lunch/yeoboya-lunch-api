67f09a46fbd2d249526c0f5480958b7fcc836d4e
kimhyunjin <khjzzm@gmail.com>
2024-03-18 10:34:02 +0900

    Refactor deploy.sh for efficiency and simplicity
    Changes in the deployment script aim to speed up and simplify the process. The JAR_NAME is now directly determined from the repository instead of a subfolder, and the chmod operation has been removed, trusting that the incoming JAR files would already have the necessary permissions.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,20 +3,17 @@
 # Repository 저장경로 설정
 REPOSITORY=/home/ec2-user/app
 
-echo -e "\n> 현재 구동중인 애플리케이션 PID 확인"
 CURRENT_PID=$(pgrep -f yeoboya-lunch-api)
-echo -e "> 현재 구동중인 애플리케이션 PID: $CURRENT_PID"
 
 # 현재 구동중인 애플리케이션이 있을 경우 종료
 if [ -z $CURRENT_PID ]; then
     echo -e "\n> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
+    echo -e "\n> 현재 구동중인 애플리케이션 PID 확인"
     echo -e "\n> kill -15 $CURRENT_PID"
     kill -15 $CURRENT_PID
     sleep 5
 fi
-
-echo -e "\n> 새 어플리케이션 배포"
 
 # Build 파일 복사
 echo -e "\n> Build 파일 복사"
@@ -27,6 +24,6 @@ JAR_NAME=$(ls -tr $REPOSITORY/ |grep 'yeoboya-lunch-api' | tail -n 1)
 echo -e "\n> JAR_NAME: $JAR_NAME"
 
 # JAR 실행
-nohup java -jar -Dspring.profiles.active=prod $REPOSITORY/jar/$JAR_NAME &
+nohup java -jar -Dspring.profiles.active=prod $REPOSITORY/$JAR_NAME > $REPOSITORY/logs/app.log 2>&1 &
 
 echo -e "\n> JAR 파일 실행 완료"
