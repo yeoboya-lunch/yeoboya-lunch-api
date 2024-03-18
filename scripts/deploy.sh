86fe710afb1a42a6954d46d152a51cfcc6ba7cda
kimhyunjin <khjzzm@gmail.com>
2024-03-18 11:10:14 +0900

    Update deploy.sh for improved logging and readability
    The deploy.sh script has been revised for better logging, enhanced readability, and simplification. An addition of 'DATE' variable is seen, which will give logs distinct names according to dates for easier tracking. Furthermore, elimination of redundant echoing and more direct approach for checking the 'yeoboya-lunch-api' application has enhanced overall script clarity and efficiency.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,28 +2,30 @@
 
 # Repository 저장경로 설정
 REPOSITORY=/home/ec2-user/app
+LOG=/home/ec2-user/logs
 
+echo "> pgrep -f yeoboya-lunch-api"
 CURRENT_PID=$(pgrep -f yeoboya-lunch-api)
 
 # 현재 구동중인 애플리케이션이 있을 경우 종료
 if [ -z $CURRENT_PID ]; then
-    echo -e "\n> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-    echo -e "\n> 현재 구동중인 애플리케이션 PID 확인"
-    echo -e "\n> kill -15 $CURRENT_PID"
+    echo "> kill -15 $CURRENT_PID sleep 5"
     kill -15 $CURRENT_PID
     sleep 5
 fi
 
 # Build 파일 복사
-echo -e "\n> Build 파일 복사"
+echo "> cp $REPOSITORY/build/libs/*.jar $REPOSITORY/"
 cp $REPOSITORY/build/libs/*.jar $REPOSITORY/
 
 # JAR 실행 파일 이름 확인
 JAR_NAME=$(ls -tr $REPOSITORY/ |grep 'yeoboya-lunch-api' | tail -n 1)
-echo -e "\n> JAR_NAME: $JAR_NAME"
+echo "> JAR_NAME: $JAR_NAME"
 
 # JAR 실행
-nohup java -jar -Dspring.profiles.active=prod $REPOSITORY/$JAR_NAME > $REPOSITORY/logs/app.log 2>&1 &
+DATE=$(date '+%Y-%m-%d')
+nohup java -jar -Dspring.profiles.active=prod $REPOSITORY/$JAR_NAME > $LOG/app-$DATE.log 2>&1 &
 
-echo -e "\n> JAR 파일 실행 완료"
+echo "> JAR 파일 실행 완료"
